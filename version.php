32193299560ae8282bcf87ba311413a26e4eedc2
Alexander Bias <bias@alexanderbias.de>
2022-11-26 21:56:35 +0100

    Release v3.10-r2

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_styles';
-$plugin->version = 2020110900;
-$plugin->release = 'v3.10-r1';
+$plugin->version = 2020110901;
+$plugin->release = 'v3.10-r2';
 $plugin->requires = 2020110900;
 $plugin->supported = [310, 310];
 $plugin->maturity = MATURITY_STABLE;
