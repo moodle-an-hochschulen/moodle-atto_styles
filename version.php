4d9e540dcbfe25092f382a6d82d65a9acca8bdf8
Alexander Bias <alexander.bias@lernlink.de>
2024-08-21 06:12:19 +0200

    Release v4.1-r4

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'atto_styles';
-$plugin->version = 2023010502;
-$plugin->release = 'v4.1-r3';
+$plugin->version = 2023010503;
+$plugin->release = 'v4.1-r4';
 $plugin->requires = 2022112800;
 $plugin->supported = [401, 401];
 $plugin->maturity = MATURITY_STABLE;
